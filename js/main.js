825c8ed4f06ba9a2bf76f5432f588d01a45b9e05
Tommy Forte <forte.39@osu.edu>
2015-10-07 20:08:07 -0400

    Added parsing functionality from text string

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
   // Steps
   // 1 - convert csv into two arrays of nodes and vertexs
   //    a - load csv (idea: user drag&drop)
-  //    b - split list by ',' delimiter
-  //    c - search for nodes (O(n) time)
-  //    d - search for edges (already have: use names as index for speed???)
-  //    e - ensure data is compatiable with D3 (can D3 do this???)
-  // 2 - set up D3 to display on screen
-  //    a - set display size
-  //    b - set default values found in demo
-  //    c - set the data to D3 object
+  //    b - DONE split list by ',' delimiter
+  //    c - DONE search for nodes (O(n) time)
+  //    d - DONE search for edges (already have: use names as index for speed???)
+  //    e - DONE ensure data is compatiable with D3 (can D3 do this???)
+  // 2 - DONE set up D3 to display on screen
+  //    a - DONE set display size
+  //    b - DONE default values found in demo
+  //    c - DONE the data to D3 object
   // 3 - Main objective complete now make it look pretty
   //    a - position and scale graph to screen
   //    b - add color
@@ -17,35 +17,42 @@
   // 4 - Bonus objectives (if time permits)
   // 5 - Pretty up and segment code to look good because I should have been doing it the whole time
 
+  var dataStream = "source,target,distance\n" + "Frolia,Hailea,9\nHailea,Hanalei,5\nHanalei,Maeulia,6\nHauauai,Lainea,8\nKaleola,Maeulia,7\nLainea,Hailea,5\nLakua,Hauauai,3\nMaeulia,Hailea,12\nPaukaa,Hauauai,6\nPoipu,Paukaa,9\nHailea,Waimea,4\nWaimea,Lakua,9\nLakua,Poipu,7\nWaimea,Kaleola,4\nMaeulia,Paukaa,14\nHailea,Lainea,8";
   var vertexs = [];
   var count = 0;
+  var width = 960;
+  var height = 600;
+  var forceCharge = -5000;
+  var nodeRadius = 10;
+  var linkDistanceScalar = 1;
 
-  var edges = d3.csv.parse("source,target,distance\nhawaii,mawii,4\ncoffee,lattee,10", function(row) {
+  var isInList = function(list, x) {
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].name === x) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  var getIndexOfVert = function(list, vertName){
+    if (isInList(vertexs, vertName) < 0){
+      vertexs.push({
+        name: vertName,
+        index: count
+      });
+      return count++;
+    } else {
+      return isInList(vertexs, vertName);
+    }
+  }
+
+  var edges = d3.csv.parse(dataStream, function(row) {
     row.distance = +row.distance;
-
-    if (!vertexs[row.source]){
-      vertexs.push({
-        name: row.source,
-        index: count
-      });
-      row.source = count;
-      count++;
-    }
-
-    if (!vertexs[row.target]){
-      vertexs.push({
-        name: row.target,
-        index: count
-      });
-      row.target = count;
-      count++;
-    }
-
+    row.source = getIndexOfVert(vertexs, row.source);
+    row.target = getIndexOfVert(vertexs, row.target);
     return row;
   });
-
-  var width = 960,
-      height = 600;
 
   var svg = d3.select("body").append("svg")
       .attr("width", width)
@@ -57,29 +64,39 @@
       .size([width, height])
       .nodes(vertexs)
       .links(edges)
-      .linkDistance(30)
-      .charge(-80)
+      .linkDistance(function(d){ return d.distance * linkDistanceScalar;})
+      .charge(forceCharge)
       .start();
 
-  var link = svg.selectAll(".link")
-      .data(edges)
-      .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+    var link = svg.selectAll(".link")
+        .data(edges)
+        .enter().append("line")
+        .attr("class", "link")
 
-  var node = svg.selectAll(".node")
-      .data(vertexs)
-      .enter().append("circle")
-      .attr("class", "node init")
-      .attr("r", 10);
+    var node = svg.selectAll(".node")
+        .data(vertexs)
+        .enter().append("circle")
+        .attr("class", "node init")
+        .attr("r", nodeRadius);
 
-  force.on("tick", function() {
-      node.attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
+    var text = svg.selectAll(".text")
+        .data(vertexs)
+        .enter().append("text")
+        .attr('x', function(d) { return d.x; })
+        .attr('y', function(d) { return d.y; })
+        .attr('class', 'text init')
+        .text(function(d) { return d.name; });
 
-      link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-  });
+    force.on("tick", function() {
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        text.attr("x", function(d) { return d.x - 30; })
+            .attr("y", function(d) { return d.y - 20; });
+    });
 })();
