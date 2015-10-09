d5fe9c4f0709bcf38e0597833867a5b71f8a5175
Tommy Forte <forte.39@osu.edu>
2015-10-09 01:02:23 -0400

    Implemented custom shortest path

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
 (function(){
-  // Steps
-  // 1 - convert csv into two arrays of nodes and vertexs
-  //    a - load csv (idea: user drag&drop)
-  //    b - DONE split list by ',' delimiter
-  //    c - DONE search for nodes (O(n) time)
-  //    d - DONE search for edges (already have: use names as index for speed???)
-  //    e - DONE ensure data is compatiable with D3 (can D3 do this???)
-  // 2 - DONE set up D3 to display on screen
-  //    a - DONE set display size
-  //    b - DONE default values found in demo
-  //    c - DONE the data to D3 object
-  // 3 - Main objective complete now make it look pretty
-  //    a - position and scale graph to screen
-  //    b - add color
-  //    c - make sure its easy to understand
-  // 4 - Bonus objectives (if time permits)
-  // 5 - Pretty up and segment code to look good because I should have been doing it the whole time
 
   var dataStream = "source,target,distance\n" + "Frolia,Hailea,9\nHailea,Hanalei,5\nHanalei,Maeulia,6\nHauauai,Lainea,8\nKaleola,Maeulia,7\nLainea,Hailea,5\nLakua,Hauauai,3\nMaeulia,Hailea,12\nPaukaa,Hauauai,6\nPoipu,Paukaa,9\nHailea,Waimea,4\nWaimea,Lakua,9\nLakua,Poipu,7\nWaimea,Kaleola,4\nMaeulia,Paukaa,14\nHailea,Lainea,8";
   var vertexs = [];
@@ -28,6 +11,82 @@
   var start = null;
   var end = null;
 
+  var thereAreUndiscoveredNodes = function (array) {
+    for (var i = 0; i < array.length; i ++) {
+      if (!array[i].discovered) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  var indexOfLowCostUndiscovered  = function (array) {
+    for (var i = array.length - 1 ; i >= 0; i-- ){
+      if (!array[i].discovered){
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  // borrowed function from the internet
+  function functiontofindIndexByKeyValue(arraytosearch, key, valuetosearch) {
+    for (var i = 0; i < arraytosearch.length; i++) {
+      if (arraytosearch[i][key] == valuetosearch) {
+        return i;
+      }
+    }
+    return null;
+  };
+
+  var shortestPath = function (nodes, edges, s, e) {
+    // set all nodes cost to infinity
+    for (var i = 0; i < nodes.length; i++) {
+      nodes[i].cost = 99999;
+      nodes[i].discovered = false;
+    }
+
+    // set the starting node cost to 0
+    for (var i = 0; i < nodes.length; i++) {
+      if (nodes[i].name === s) {
+        nodes[i].cost = 0;
+      }
+    }
+
+    // sort array by cost
+    nodes.sort(function(a,b){ return b.cost - a.cost});
+
+    while (thereAreUndiscoveredNodes(nodes) != -1) {
+      var tmp = nodes[indexOfLowCostUndiscovered(nodes)];
+      tmp.discovered = true;
+      //found the path
+      if (tmp.name === e && tmp.cost != 99999) {
+        var path = [];
+        path.push(e);
+        tmp = nodes[functiontofindIndexByKeyValue(nodes,"name", tmp.prev)];
+        while (tmp.name != s) {
+          path.push(tmp.name);
+          tmp = nodes[functiontofindIndexByKeyValue(nodes,"name", tmp.prev)];
+        }
+        path.push(tmp.name);
+        return path;
+      }
+
+      // search for paths
+      for (var i = 0; i < edges.length; i++){
+        if (edges[i].source.name === tmp.name) {
+          if (edges[i].distance + tmp.cost < nodes[functiontofindIndexByKeyValue(nodes,"name",edges[i].target.name)].cost){
+            nodes[functiontofindIndexByKeyValue(nodes,"name",edges[i].target.name)].cost = edges[i].distance + tmp.cost;
+            nodes[functiontofindIndexByKeyValue(nodes,"name",edges[i].target.name)].prev = tmp.name;
+          }
+        }
+      }
+      // resort
+      nodes.sort(function(a,b){ return b.cost - a.cost});
+    }
+    return ["No path found!"];
+  };
+
   var isInList = function(list, x) {
     for (var i = 0; i < list.length; i++) {
       if (list[i].name === x) {
@@ -35,7 +94,7 @@
       }
     }
     return -1;
-  }
+  };
 
   var getIndexOfVert = function(list, vertName){
     if (isInList(vertexs, vertName) < 0){
@@ -47,6 +106,15 @@
     } else {
       return isInList(vertexs, vertName);
     }
+  };
+
+  var printResults = function (array) {
+    var str = "";
+    while (array.length > 1) {
+      str += array.pop() + " -> ";
+    }
+    str += array.pop();
+    return str
   }
 
   var onNodeClick = function (d) {
@@ -65,9 +133,11 @@
         d3.select(".end").classed("end", false);
         d3.select(this).classed("end", true);
         end = this;
+        result = shortestPath(force.nodes(), force.links(), start.__data__.name, end.__data__.name);
+        d3.select("text").text(printResults(result));
       }
     }
-  }
+  };
 
   var edges = d3.csv.parse(dataStream, function(row) {
     row.distance = +row.distance;
@@ -105,6 +175,13 @@
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
 
+    var result = svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", (height / 7) * 6 )
+        .attr("text-anchor", "middle")
+        .style("font-size", "20px")
+        .text(" ");
+
     // make path with arrow tip
     var path = svg.append("svg:g").selectAll("path")
         .data(force.links())
@@ -120,7 +197,7 @@
         .attr("r", 10)
         .on("click", onNodeClick);
 
-    // draw text on nodes
+    // make text
     var text = svg.selectAll(".text")
       .data(force.nodes())
       .enter().append("text")
@@ -142,6 +219,6 @@
       	    return "translate(" + d.x + "," + d.y + ")"; });
 
         text.attr("x", function(d) { return d.x + 12; })
-        .attr("y", function(d) { return d.y + 5; });
+            .attr("y", function(d) { return d.y + 5; });
     }
 })();
